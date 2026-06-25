7a5d0ef4d3ec42929dd819b123ebaac120d9149e
Anuja Ingle <ingleanuja1@gmail.com>
2026-06-25 10:32:05 +0530

    Inital commit

---
+++ Java_Assignment/Assign1_StoreData.java
@@ -0,0 +1,24 @@
+package Java_Assignment;
+
+public class Assign1_StoreData {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		  // Storing student details
+        int studentId = 101;
+        char grade = 'A';
+        boolean passed = true;
+        int age = 18;
+        
+        
+     // Displaying student details
+        System.out.println("Student ID: " + studentId);
+        System.out.println("Grade: " + grade);
+        System.out.println("Passed: " + passed);
+        System.out.println("Age: " + age);
+
+ }
+
+}
+
